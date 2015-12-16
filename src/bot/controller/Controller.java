04a815f2a952f7caf7970c9a452bb6ddab75fbd5
JordanBurnett1210 <jabur1210@gmail.com>
2015-12-16 08:48:21 -0700

    Created robot project

---
+++ src/bot/controller/Controller.java
@@ -0,0 +1,10 @@
+package bot.controller;
+
+public class Controller
+{
+	public void start()
+	{
+		
+	}
+
+}
---
+++ src/bot/controller/JordanRunner.java
@@ -0,0 +1,12 @@
+package bot.controller;
+
+public class JordanRunner
+{
+
+	public static void main(String[] args)
+	{
+		Controller myController = new Controller();
+		myController.start();
+	}
+
+}
